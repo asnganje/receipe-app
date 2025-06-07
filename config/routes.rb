b3768047ebd775dd3335a44a3eb0a7b2a32ea084
asnganje <abd.nganje@gmail.com>
2025-06-07 14:46:08 +0300

    fix linter errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :recipes, only: [:index]
+  resources :recipes, only: [ :index ]
   resources :dashboard, only: [ :index ]
   devise_for :users
   get "up" => "rails/health#show", as: :rails_health_check
