16e2b34211c583e8dffcf0086c526dca4cad2183
asnganje <abd.nganje@gmail.com>
2025-06-07 17:45:12 +0300

    add create and new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :recipes, only: [ :index ]
-  resources :foods, only: [:index]
+  resources :foods, only: [:index, :create, :new]
   resources :dashboard, only: [ :index ]
   devise_for :users
   get "up" => "rails/health#show", as: :rails_health_check
