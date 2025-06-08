4819e70d3504c6209c0b6bcd5a3536a6afd9db99
asnganje <abd.nganje@gmail.com>
2025-06-08 18:29:19 +0300

    add recipe create action

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,4 +3,25 @@ class RecipesController < ApplicationController
   def index
     @recipes = Recipe.all
   end
+
+  def new
+  end
+
+  def create
+    @recipe = Recipe.create(recipe_params)
+    if @recipe.save
+      respond_to do |format|
+        format.html {redirect_to recipes_url, notice:"Recipe added!"}
+      end
+    else
+    respond_to do |format|
+        format.html {redirect_to recipes_url, alert:@recipe.errors.full_messages[0]}
+      end
+    end
+  end
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(:name, :description, :preparation_time, :cooking_time, :public)
+  end 
 end
