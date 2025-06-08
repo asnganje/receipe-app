e9339427f6857af3b9a09f47ed0f308023c870c5
asnganje <abd.nganje@gmail.com>
2025-06-08 18:22:55 +0300

    remove fake recipe data

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,9 +1,6 @@
 class RecipesController < ApplicationController
   layout "admin"
   def index
-    @recipes = []
-    10.times.each do |i|
-      @recipes << "recipe#{i}"
-    end
+    @recipes = Recipe.all
   end
 end
