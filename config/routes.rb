d4950c5dfda5ad4b8b13fa6f45dc62677a0fc036
asnganje <abd.nganje@gmail.com>
2025-06-09 15:08:04 +0300

    add shopping list pop up logic

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get "shopping_list/index"
+  resources :shopping_list, only: [:new]
+  post "shopping_lists/generate", to: "shopping_lists#generate", as: :generate_shopping_list
   resources :recipes do
     resources :recipe_foods
   end
