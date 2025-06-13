45fec853c4dddceb866ba61f17a5b2c1d4bf65cb
asnganje <abd.nganje@gmail.com>
2025-06-13 10:10:50 +0300

    fix linter errors

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,8 +1,8 @@
 class Food < ApplicationRecord
-  has_many :shopping_list_items, dependent: :nullify
+  has_many :shopping_list_items, dependent: :destroy
   has_many :recipe_foods, dependent: :destroy
   has_many :recipes, through: :recipe_foods
-  has_many :inventory_foods
+  has_many :inventory_foods, dependent: :destroy
   has_many :inventories, through: :inventory_foods
   validates :name, presence: true
   validates :measurement_unit, presence: true
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,7 @@
 class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods
+  has_many :shopping_lists, dependent: :destroy
   has_many :foods, through: :recipe_foods
 
   def preparation_duration
